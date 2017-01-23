fb0584d823bd17124c1e78c1aabfdfe610febd36
prateek <prateekp987@gmail.com>
2017-01-23 14:44:43 +0530

    Food Class updated

--- app/src/main/java/com/example/prateek/bimsapp/Food.java
+++ app/src/main/java/com/example/prateek/bimsapp/Food.java
@@ -9,33 +9,43 @@ public class Food {
         public Food() {
         }
 
-        public Food(String food, String price, String imageUrl) {
+        public Food(String food, String price, String imageUrl, String rating, String availability) {
             this.food = food;
             this.price = price;
             this.imageUrl = imageUrl;
+            this.rating = rating;
+            this.availability= availability;
         }
 
 
 
         public String getFood() {
-            return food;
+        return food;
         }
 
         public void setFood(String name) {
-            this.food = name;
+        this.food = name;
         }
 
         public String getPrice() {
-            return price;
+        return price;
         }
 
         public void setPrice(String price) {
-            this.price = price;
+        this.price = price;
         }
 
         public void setImageUrl(String imageUrl){this.imageUrl = imageUrl;}
 
         public String getImageUrl(){ return  imageUrl;}
+
+        public void setRating(String rating){this.rating = rating;}
+
+        public String getRating(){ return rating;}
+
+        public void setAvailability(String availability){this.availability = availability;}
+
+        public String getAvailability(){ return availability;}
 
 
 
