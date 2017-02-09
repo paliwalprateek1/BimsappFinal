9387f02688a80c90c3e172ae32a753c3c5ebf533
Prateek Paliwal <prateekp987@gmail.com>
2017-02-10 01:36:10 +0530

    Delete Server.java

--- app/src/main/java/com/example/prateek/bimsapp/Server.java
+++
@@ -1,8 +0,0 @@
-package com.example.prateek.bimsapp;
-
-/**
- * Created by prateek on 12/10/16.
- */
-public class Server {
-    final static String URL =  "https://bimsapp-81da4.firebaseio.com/";
-}
