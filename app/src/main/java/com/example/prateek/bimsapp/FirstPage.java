0b3005ffd72d5040c6e1cca1628720d307735f7c
prateek <prateekp987@gmail.com>
2017-04-12 06:04:54 +0530

    Launch Mode singleTop error fixed.

--- app/src/main/java/com/example/prateek/bimsapp/FirstPage.java
+++ app/src/main/java/com/example/prateek/bimsapp/FirstPage.java
@@ -58,7 +58,7 @@ public class FirstPage extends AppCompatActivity {
                     // This method will be executed once the timer is over
                     // Start your app main activity
                    // if(StoreSharedPreferences.getUserEmail(getApplicationContext()).length()!=0) {
-                        Intent i = new Intent(FirstPage.this, Login.class);
+                        Intent i = new Intent(FirstPage.this, SelectRestraunt.class);
                         startActivity(i);
 //                    }
 //                    else{
